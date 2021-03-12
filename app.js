3f6fc38e331d41beed0f953e05d24dcef96992d8
Michael Bowman <micbowman25@gmail.com>
2021-03-12 16:10:12 -0600

    finished and shortened code

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
-const displayKey = document.querySelector('.key');
-const displayKeyCode = document.querySelector('.keyCode');
-const keyCodeDiv = document.querySelector('.keyCode');
-const overlay = document.querySelector('.overlay');
+const insert = document.querySelector('#insert');
 
-window.addEventListener('keydown', (e) => {
-    overlay.classList.add('hide');
-    displayKey.innerText = e.key;
-    displayKeyCode.innerText = e.keyCode;
-    if(e.keyCode === 32){
-        displayKey.innerText = `'space'`;
-    }
+window.addEventListener("keydown", (event) => {
+    insert.innerHTML = `
+    <div class="key">
+    ${event.key === " " ? "Space" : event.key}
+    <small>event.key</small>
+    </div>
+
+    <div class="key">
+    ${event.keyCode}
+    <small>event.keyCode</small>
+    </div>
+
+    <div class="key">
+    ${event.code}
+    <small>event.code</small>
+    </div>
+    `;
 });
-
-keyCodeDiv.addEventListener('click', (e) => {
-    const textArea = document.createElement('textarea');
-    textArea.setAttribute('readonly', '');
-    textArea.style.position = 'absolute';
-    textArea.value = keyCodeDiv.querySelector('h2').innerText;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textArea);
-    keyCodeDiv.querySelector('p').innerText = 'Copied';
-    setTimeout(() => {
-        keyCodeDiv.querySelector('p').innerText = 'Click To Copy';
-    },2000)
-})
